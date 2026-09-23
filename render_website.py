f6a88945929399d2af1f5dbf08851c39fc38e843
VASILIYKAS <yoyoynv@gmail.com>
2026-09-23 18:21:48 +0500

    Add links to books txt files

--- render_website.py
+++ render_website.py
@@ -1,7 +1,15 @@
 import json
+import os
+
+from urllib.parse import quote
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
+
+
+def make_slug(book_path):
+    name = os.path.splitext(os.path.basename(book_path))[0]
+    return name.split('-', 1)[0]
 
 
 def render_books_page(books_file='meta_data.json', template_name='base.html'):
@@ -12,6 +20,11 @@ def render_books_page(books_file='meta_data.json', template_name='base.html'):
 
     left_column = chunks[0]
     right_column = chunks[1]
+
+    for book in books:
+        encoded_path = quote(book['book_path'], safe='/')
+        book['book_path'] = f"/{encoded_path}"
+        book['slug'] = make_slug(book['book_path'])
 
     env = Environment(
         loader=FileSystemLoader('.'),
