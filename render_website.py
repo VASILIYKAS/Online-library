d0a0744b1ecabcef92d5137c4ffb393f21e1c393
VASILIYKAS <yoyoynv@gmail.com>
2026-09-24 13:37:30 +0500

    Split books list by pages

--- render_website.py
+++ render_website.py
@@ -12,18 +12,13 @@ def make_slug(book_path):
     return name.split('-', 1)[0]
 
 
-def render_books_page(books_file='meta_data.json', template_name='base.html'):
+def render_books_page(books_file='meta_data.json', template_name='base.html', books_per_page=10):
     with open(books_file, 'r', encoding='utf-8') as file:
         books = json.load(file)
 
-    chunks = list(chunked(books, len(books) // 2))
-
-    left_column = chunks[0]
-    right_column = chunks[1]
-
     for book in books:
         encoded_path = quote(book['book_path'], safe='/')
-        book['book_path'] = f"/{encoded_path}"
+        book['read_url'] = f"/{encoded_path}"
         book['slug'] = make_slug(book['book_path'])
 
     env = Environment(
@@ -31,30 +26,44 @@ def render_books_page(books_file='meta_data.json', template_name='base.html'):
         autoescape=select_autoescape(['html', 'xml'])
     )
 
+    chunks = list(chunked(books, books_per_page))
+    total_pages = len(chunks)
+
+    if not os.path.exists('pages'):
+        os.makedirs('pages')
+
     template = env.get_template(template_name)
 
-    rendered_page = template.render(
-        left_column=left_column,
-        right_column=right_column,
-    )
+    page_numbers = range(1, total_pages + 1)
+
+    for i, page_books in enumerate(chunks):
+        page_number = i + 1
+        
+        rendered_page = template.render(
+            books=page_books,
+            current_page=page_number,
+            total_pages=total_pages,
+            page_numbers=page_numbers,
+            prev_page=page_number - 1 if page_number > 1 else None,
+            next_page=page_number + 1 if page_number < total_pages else None
+        )
+
+        filename = f'pages/index{page_number}.html'
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(rendered_page)
 
     return rendered_page
 
 
 def main():
-    page = render_books_page()
-
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(page)
+    render_books_page()
 
     server = Server()
 
     server.watch('base.html')
 
     def on_reload():
-        page = render_books_page()
-        with open('index.html', 'w', encoding="utf8") as file:
-            file.write(page)
+        render_books_page()
 
     server.watch('base.html', on_reload)
 
